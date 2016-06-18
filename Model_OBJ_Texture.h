de2c9bcc91c90c27a3ef63e1f0c4230257d1a2b2
ahnjaewoo <ahnpersie@gmail.com>
2016-06-18 19:07:29 +0900

    Update Model_OBJ_Texture.h

--- Model_OBJ_Texture.h
+++ Model_OBJ_Texture.h
@@ -1,8 +1,16 @@
 #pragma once
+#include<vector>
+#include<iostream>
+//#include<glew.h>
+#include<gl/glut.h>
+#include<gl/GL.h>
+#include<gl/GLU.h>
+#include<string>
+using namespace std;
 class Model_OBJ_Texture
 {
 public:
-	float* normals;		//stores the normal
+	/*float* normals;		//stores the normal
 	float* Faces_Triangles;	//stores the triangles
 	float* Faces_Triangles_vertex_normal;
 	float* Faces_Triangles_uv;
@@ -16,13 +24,54 @@ public:
 	long TotalConnectedQuads;
 	long TotalConnectedTriangles; //stores the total numer of connected triangles
 	long TotalConnectedVertexNormals;
-	long TotalConnectedUvs;
+	long TotalConnectedUvs;*/
+	class Vertex {
+	public:
+		float x;
+		float y;
+		float z;
+		Vertex();
+		~Vertex();
+		void operator =(Vertex v);
+	};
+	class UV {
+	public:
+		float x;
+		float y;
+		UV();
+		~UV();
+	};
 
+	class Face {
+	public:
+		vector<Vertex> face_vList;
+		vector<Vertex> face_nList;
+		vector<UV> face_uvList;
+		Face();
+		~Face();
+	};
+	class SubMesh {
+	public:
+		vector<Face> subMesh_fList;
+		float Kd[4], Ks[4], Ka[4];
+		string loadFile;
+		GLuint sub_texture;
+		SubMesh();
+		~SubMesh();
+		void setMaterial();
+	};
+	vector<SubMesh> Mesh_subList;
+	vector<Vertex> vList;
+	vector<Vertex> nList;
+	vector<UV> uvList;
+	int numOfV,numOfN,numOfF,numOfSub,numOfUv;
+	string MtlFileName;
+	
 	Model_OBJ_Texture();
 	~Model_OBJ_Texture();
-	float* calculateNormal(float* coord1, float* coord2, float* coord3);
 	int Load(char *filename);
 	void Draw();
 	void Release();
 };
 
+bool LoadMeshFromFile(string loadFile, GLuint &tex_id);
